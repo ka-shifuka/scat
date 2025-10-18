4e544a6e51fd02383f40cbc612eb903ed428c199
ka-shifuka <aryadewanggapuja@gmail.com>
2025-10-19 06:05:11 +0700

    Add web

--- docs/game.js
+++ docs/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"8736d9e4-47ad-4be0-a283-ca80f9d90380","remote_package_size":400754,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":400754,"audio":false}]});
+  loadPackage({"package_uuid":"c1b94c64-fc29-46bf-a26a-2b5fbba4b12a","remote_package_size":8514811,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":8514811,"audio":false}]});
 
 })();
